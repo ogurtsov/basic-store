6150c614fd727c274a33a4a8f68c2bb24057d4e2
Dmitry Ogurtsov <zorbis11@gmail.com>
2017-10-20 15:57:45 +0300

    basic models prepared

---
+++ store/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+
+
+class Category(models.Model):
+    name_ru = models.CharField(max_length=255)
+    name_ro = models.CharField(max_length=255)
+    slug = models.SlugField()
+
+    def __str__(self):
+        return '{}:{}'.format(self.slug, self.name_ru)
+
+
+
+
+class Item(models.Model):
+    categories = models.ManyToManyField(Category)
+    related_items = models.ManyToManyField('Item', blank=True)
+    name_ru = models.CharField(max_length=255)
+    name_ro = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name_ru
